716c36b27af6afa3ab753e540f9294c8168312d9
yakine123 <yakinebenali5@gmail.com>
2024-01-18 13:12:03 +0100

    feat:add `loginUser` function

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,10 +1,10 @@
 
 
-const {  registerUser } = require('../controllers/userController');
+const {  registerUser,loginUser } = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
 
 router.post('/register',registerUser); 
-
+router.post('/login',loginUser); 
 module.exports = router;
  
