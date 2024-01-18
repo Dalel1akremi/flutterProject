63323fd13d8df2f5c2abddc049eade372013ae35
dalel <dalelakremi2020@gmail.com>
2024-01-18 12:43:40 +0100

    feat:fix `userModel`

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,10 +1,12 @@
 // models/userModel.js
 
+const { number } = require('joi');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
+  nom: String,
+  prenom: String,
+  telephone:String,
   email: String,
   password: String,
 });
