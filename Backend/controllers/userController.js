a9b013977c5dfa51c00322b799c64518297b7b64
yakine123 <yakinebenali5@gmail.com>
2024-01-23 11:43:04 +0100

    feat add api `reset password`backend

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,6 +2,8 @@
 const jwt = require('jsonwebtoken'); 
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
+const  nodemailer = require("nodemailer");
+const  crypto= require("crypto");
 
 const registerUser = async (req, res) => {
   const { nom, prenom, telephone, email, password, confirmPassword } = req.body;
@@ -75,8 +77,60 @@ const loginUser = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+const reset_password = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Recherche de l'utilisateur dans la base de données
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      // L'utilisateur n'existe pas
+      return res.status(404).json({ message: 'Utilisateur non trouvé.' });
+    }
+
+    // Générer un code de validation aléatoire à 6 chiffres
+    const validationCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    // Sauvegarder le code de validation dans la base de données
+    user.validationCode = validationCode;
+    await user.save();
+
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'yakinebenali5@gmail.com',
+        pass: 'gqyi qqac kxxz qqrn',
+      },
+    });
+   
+
+    // Envoyer le code de validation par e-mail
+    const mailOptions = {
+      from: '', // Put your email address here
+      to: email, // Change to user.email if you want to send it to the user's email
+      subject: 'Code de validation',
+      text: `Votre code de validation est : ${validationCode}`,
+      replyTo: email,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Email not sent' });
+      } else {
+        console.log('Email sent successfully: ' + info.response);
+        res.json({ success: true, message: 'Email sent successfully' });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Erreur lors de la génération du code de validation.' });
+  }
+};
 
 module.exports = {
   registerUser,
   loginUser,
+  reset_password,
 };
