bf8aedc770006991004b96fca13fd80179442cd0
yakine123 <yakinebenali5@gmail.com>
2024-01-29 10:30:43 +0100

    feat :add fonction `insererComposition`dans le file `CompositionDeBController`

---
+++ Backend/controllers/CompositionDeBController.js
@@ -0,0 +1,35 @@
+const Composition = require('../models/CompositionDeBase');
+
+const insererComposition = async (req, res) => {
+    const { nom, image } = req.body;
+  
+    try {
+        const compositionExistante = await Composition.findOne({ nom });
+
+        if (compositionExistante) {
+          // Envoyer une réponse d'erreur si le nom existe déjà
+          return res.status(400).json({ error: 'Le nom de composition existe déjà' });
+        }
+    
+      const nouvelleComposition = new Composition({
+        nom,
+        image,
+      });
+  
+      // Sauvegarder l'instance dans la base de données
+      const compositionEnregistree = await nouvelleComposition.save();
+  
+      console.log('Composition enregistrée avec succès:', compositionEnregistree);
+  
+      // Envoyer une réponse réussie
+      res.status(200).json({ message: 'Composition enregistrée avec succès', data: compositionEnregistree ,Statut:res.statusCode});
+    } catch (erreur) {
+      console.error('Erreur lors de l\'enregistrement de la composition:', erreur.message);
+  
+      // Envoyer une réponse d'erreur
+      res.status(500).json({ error: 'Erreur lors de l\'enregistrement de la composition' });
+    }
+  };
+  
+  module.exports = { insererComposition };
+  
