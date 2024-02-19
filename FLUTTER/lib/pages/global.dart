d39ebe0b853e9d62a3dc6e7533a11e750246855a
dalel <dalelakremi2020@gmail.com>
2024-02-19 09:37:06 +0100

    feat:add `global` dart file to global var

---
+++ FLUTTER/lib/pages/global.dart
@@ -0,0 +1,46 @@
+import 'aboutRestaurant/acceuil.dart';
+
+class Panier {
+  static final Panier _instance = Panier._internal();
+  factory Panier() => _instance;
+
+  Panier._internal();
+  
+
+  List<Article> articles = [];
+ 
+
+  void ajouterAuPanier1(Article article) {
+    articles.add(article);
+   
+  }
+ 
+  double getTotalPrix() {
+    double total = 0;
+    for (var articles in articles) {
+      total += articles.prix * articles.quantite;
+    }
+   
+    return total;
+  }
+}
+
+class Article {
+  int id_item;
+  String nom;
+  String img;
+  int prix;
+  Restaurant restaurant;
+
+  int quantite;
+
+  Article({
+    required this.id_item,
+    required this.nom,
+    required this.img,
+    required this.prix,
+    required this.restaurant,
+    required this.quantite,
+  });
+}
+
