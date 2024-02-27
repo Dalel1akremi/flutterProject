d0f3f6eacb62a1e8eda08cd7e5f7d92e93dbc834
dalel <dalelakremi2020@gmail.com>
2024-02-27 17:02:35 +0100

    feat: fix `affiche list` with `button` and add `remarque` row in `StepMenuPage`

--- FLUTTER/lib/pages/aboutRestaurant/stepMenuPage.dart
+++ FLUTTER/lib/pages/aboutRestaurant/stepMenuPage.dart
@@ -68,19 +68,19 @@ class _StepMenuPageState extends State<StepMenuPage> {
         title: Text(widget.nom),
         backgroundColor: const Color.fromARGB(222, 212, 133, 14),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              widget.img,
-              width: 300,
-              height: 250,
-            ),
-            const SizedBox(height: 20),
-            
-            Expanded(
-              child: ListView.builder(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                widget.img,
+                width: 300,
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              ListView.builder(
+                shrinkWrap: true,
                 itemCount: stepNames.length,
                 itemBuilder: (BuildContext context, index) {
                   return ElevatedButton(
@@ -96,8 +96,8 @@ class _StepMenuPageState extends State<StepMenuPage> {
                               Spacer(),
                               IconButton(
                                 icon: afficherListe
-                                    ? Icon(Icons.arrow_drop_up)
-                                    : Icon(Icons.arrow_drop_down),
+                                    ? const Icon(Icons.arrow_drop_up)
+                                    : const Icon(Icons.arrow_drop_down),
                                 onPressed: () {
                                   setState(() {
                                     afficherListe = !afficherListe;
@@ -124,11 +124,11 @@ class _StepMenuPageState extends State<StepMenuPage> {
                   );
                 },
               ),
-            ),
-            const SizedBox(height: 20),
-            if (afficherListe)
-              Expanded(
-                child: ListView.builder(
+              
+              const SizedBox(height: 20),
+              if (afficherListe)
+                ListView.builder(
+                  shrinkWrap: true,
                   itemCount: itemNames.length,
                   itemBuilder: (BuildContext context, int index) {
                     return ListTile(
@@ -145,100 +145,135 @@ class _StepMenuPageState extends State<StepMenuPage> {
                     );
                   },
                 ),
+
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Remarque: ",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 300,
+                        height: 100,
+                        child: Card(
+                          elevation: 2,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              controller: _remarkController,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Entrez une remarque',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _value = _value > 1 ? _value - 1 : 1;
-                    });
-                  },
-                  icon: const Icon(Icons.remove),
-                  iconSize: 30,
-                ),
-                Text(
-                  '$_value',
-                  style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _value++;
-                    });
-                  },
-                  icon: const Icon(Icons.add),
-                  iconSize: 30,
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 1000,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Create article object
-                  Article article = Article(
-                    id_item: widget.id_item,
-                    nom: widget.nom,
-                    img: widget.img,
-                    prix: widget.prix,
-                    quantite: _value,
-                  );
-
-                  // Add article to cart
-                  Panier().ajouterAuPanier1(article);
-
-                  // Navigate to next page
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => NextPage(
-                        panier: Panier().articles,
-                      ),
-                      settings: RouteSettings(
-                        arguments: {'article': article},
-                      ),
-                    ),
-                  ).then((result) {
-                    if (result != null) {
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
                       setState(() {
-                        article.quantite = result['numberOfItems'];
-                        article.prix = result['totalPrice'];
+                        _value = _value > 1 ? _value - 1 : 1;
                       });
-                    }
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Ajouter $_value article${_value != 1 ? 's' : ''}',
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                    },
+                    icon: const Icon(Icons.remove),
+                    iconSize: 30,
+                  ),
+                  Text(
+                    '$_value',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _value++;
+                      });
+                    },
+                    icon: const Icon(Icons.add),
+                    iconSize: 30,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 1000,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Create article object
+                    Article article = Article(
+                      id_item: widget.id_item,
+                      nom: widget.nom,
+                      img: widget.img,
+                      prix: widget.prix,
+                      quantite: _value,
+                    );
+
+                    // Add article to cart
+                    Panier().ajouterAuPanier1(article);
+
+                    // Navigate to next page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NextPage(
+                          panier: Panier().articles,
+                        ),
+                        settings: RouteSettings(
+                          arguments: {'article': article},
+                        ),
                       ),
-                    ),
-                    Text(
-                      ' $totalPrice £',
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                    ).then((result) {
+                      if (result != null) {
+                        setState(() {
+                          article.quantite = result['numberOfItems'];
+                          article.prix = result['totalPrice'];
+                        });
+                      }
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Ajouter $_value article${_value != 1 ? 's' : ''}',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        ' $totalPrice £',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
