558c9442b0bd8b9fd3466d5c18dcf4fc11de08ae
dalel <dalelakremi2020@gmail.com>
2024-03-08 09:30:24 +0100

    feat:fix `paiement`

--- FLUTTER/lib/pages/aboutPaiement/paiement.dart
+++ FLUTTER/lib/pages/aboutPaiement/paiement.dart
@@ -1,17 +1,21 @@
 // ignore_for_file: avoid_print, library_private_types_in_public_api, use_build_context_synchronously
 import 'dart:convert';
+import 'package:demo/pages/aboutRestaurant/commande.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import './../global.dart';
 import './../aboutUser/auth_provider.dart';
+
 class CreditCard {
   final String fullCardNumber;
-  final String cvv; // Ajouter le champ CVV
+  final String cvv;
+  final String _id; // Ajouter le champ CVV
   String get maskedCardNumber {
-    return '*' * (fullCardNumber.length - 4) + fullCardNumber.substring(fullCardNumber.length - 4);
+    return '*' * (fullCardNumber.length - 4) +
+        fullCardNumber.substring(fullCardNumber.length - 4);
   }
 
-  CreditCard(this.fullCardNumber, this.cvv); // Mettre à jour le constructeur
+  CreditCard(this.fullCardNumber, this.cvv, this._id,); // Mettre à jour le constructeur
 }
 
 class PaymentScreen extends StatefulWidget {
@@ -33,129 +37,201 @@ class _PaymentScreenState extends State<PaymentScreen> {
   bool payInStore = false;
   String? selectedPaymentMethod;
   bool enableInStoreCheckbox = false;
-   List<CreditCard> userCreditCards = [];  // Variable pour activer/désactiver la case à cocher "En magasin"
- String? selectedCreditCard;
+  List<CreditCard> userCreditCards =
+      []; // Variable pour activer/désactiver la case à cocher "En magasin"
+  String? selectedCreditCard;
 // Définir une fonction pour vérifier si l'une des options de la liste de sélection radio est sélectionnée
-bool isPaymentMethodSelected() {
-  return selectedPaymentMethod != null;
-}
+  bool isPaymentMethodSelected() {
+    return selectedPaymentMethod != null;
+  }
+
   @override
   void initState() {
     super.initState();
     initAuthProvider();
     fetchUserCreditCards();
   }
-Future<void> showCVVDialog(String cvv) async {
-  String enteredCVV = ''; // Variable pour stocker le code confidentiel entré
-  final TextEditingController controller = TextEditingController();
-
-  await showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Entrez votre code confidentiel'),
-        content: TextField(
-          controller: controller,
-          keyboardType: TextInputType.number,
-          obscureText: true, // Masquer le texte saisi
-          onChanged: (value) {
-            enteredCVV = value;
-          },
-          decoration: const InputDecoration(
-            hintText: 'Code confidentiel',
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: const Text('Annuler'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pop(enteredCVV); // Renvoyer le code confidentiel entré
-            },
-            child: const Text('Valider'),
-          ),
-        ],
-      );
-    },
-  ).then((value) {
-    // Vérifiez le code confidentiel saisi après la fermeture de la boîte de dialogue
-    if (value != null && value == cvv) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Code correcte'),
-            content: const Text('Merci pour votre confiance.'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Code confidentiel incorrect'),
-            content: const Text('Veuillez réessayer !'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('OK'),
-              ),
-            ],
-          );
-        },
-      );
-    }
-  });
-}
-
-Future<void> fetchUserCreditCards() async {
-  // Retrieve user's email from AuthProvider
-  String? userEmail = authProvider.email;
-  if (userEmail != null) {
-    try {
-      var response = await http.get(Uri.parse('http://localhost:3000/recupererCartesUtilisateur?email=$userEmail'));
-      if (response.statusCode == 200) {
-        // Parse JSON response and update userCreditCards list
-        var responseData = json.decode(response.body);
-        if (responseData is Map && responseData.containsKey('cards')) {
-          setState(() {
-            userCreditCards = (responseData['cards'] as List)
-                .map((data) => CreditCard(data['cardNumber'], data['cvv']))
-                .toList();
-          });
-        } else {
-          throw Exception('Invalid response format: $responseData');
-        }
-      } else {
-        throw Exception('Failed to load user credit cards: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error fetching user credit cards: $e');
-    }
+Future<void> handlePayment() async {
+  if (useCreditCard && selectedCreditCard != null) {
+    // Make payment with credit card
+    makePaymentWithCreditCard();
+  } else if (payInStore && selectedPaymentMethod != null) {
+    
+    // Navigate to CommandePage if pay in store option is selected
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CommandeApp()),
+    );
+  } else {
+    // Show error message if payment method is not selected
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Erreur'),
+          content: const Text('Merci de vérifier votre mode de paiement avant de passer votre commande.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
+ void makePaymentWithCreditCard() async {
+  if (selectedCreditCard != null) {
+    try {
+      CreditCard selectedCard = userCreditCards.firstWhere((card) => card.fullCardNumber == selectedCreditCard);
+      // Utilisez la propriété _id de la carte sélectionnée
+      String cardId = selectedCard._id;
+      
+      // Assuming your API endpoint for payment is like this
+      var response = await http.post(
+        Uri.parse('http://localhost:3000/recupererCarteParId?email=${authProvider.email}&cardId=$cardId'),
+        body: jsonEncode({
+          'montant': panier.getTotalPrix()
+        }), // Include the total amount in the request body
+        headers: {
+          'Content-Type': 'application/json'
+        }, // Specify the content type as JSON
+      );
+      if (response.statusCode == 200) {
+        // Handle successful payment response
+        print('Payment successful!');
+      } else {
+        throw Exception('Failed to process payment: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error processing payment: $e');
+    }
+  } else {
+    print('Please select a credit card for payment.');
+  }
+   
+   Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) =>const  CommandeApp()),
+  );
+}
 
- Future<void> initAuthProvider() async {
+  Future<void> showCVVDialog(String cvv) async {
+    String enteredCVV = ''; // Variable pour stocker le code confidentiel entré
+    final TextEditingController controller = TextEditingController();
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Entrez votre code confidentiel'),
+          content: TextField(
+            controller: controller,
+            keyboardType: TextInputType.number,
+            obscureText: true, // Masquer le texte saisi
+            onChanged: (value) {
+              enteredCVV = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Code confidentiel',
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Annuler'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(enteredCVV); // Renvoyer le code confidentiel entré
+              },
+              child: const Text('Valider'),
+            ),
+          ],
+        );
+      },
+    ).then((value) {
+      // Vérifiez le code confidentiel saisi après la fermeture de la boîte de dialogue
+      if (value != null && value == cvv) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Code correcte'),
+              content: const Text('Merci pour votre confiance.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Code confidentiel incorrect'),
+              content: const Text('Veuillez réessayer !'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    });
+  }
+
+  Future<void> fetchUserCreditCards() async {
+    // Retrieve user's email from AuthProvider
+    String? userEmail = authProvider.email;
+    if (userEmail != null) {
+      try {
+        var response = await http.get(Uri.parse(
+            'http://localhost:3000/recupererCartesUtilisateur?email=$userEmail'));
+        if (response.statusCode == 200) {
+          // Parse JSON response and update userCreditCards list
+          var responseData = json.decode(response.body);
+          if (responseData is Map && responseData.containsKey('cards')) {
+            setState(() {
+              userCreditCards = (responseData['cards'] as List)
+                  .map((data) =>
+                      CreditCard(data['cardNumber'], data['cvv'], data['_id']))
+                  .toList();
+            });
+          } else {
+            throw Exception('Invalid response format: $responseData');
+          }
+        } else {
+          throw Exception(
+              'Failed to load user credit cards: ${response.statusCode}');
+        }
+      } catch (e) {
+        print('Error fetching user credit cards: $e');
+      }
+    }
+  }
+
+  Future<void> initAuthProvider() async {
     await authProvider.initTokenFromStorage();
     setState(() {});
   }
-
-
 
   String mapRetraitMode(String value) {
     switch (value) {
@@ -372,36 +448,39 @@ Future<void> fetchUserCreditCards() async {
                 useCreditCard = value ?? false;
                 if (useCreditCard) {
                   payInStore = false;
-                fetchUserCreditCards();
-                 
-                }
+                  fetchUserCreditCards();
+                }else {
+        // Réinitialiser la carte sélectionnée lorsque l'option est décochée
+        selectedCreditCard = null;
+      }
               });
             },
           ),
-           if (useCreditCard && userCreditCards.isNotEmpty)
+          if (useCreditCard && userCreditCards.isNotEmpty)
             // Inside the build method where you display the credit card list
-Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-    const Text(
-      'Sélectionnez une carte bancaire :',
-      style: TextStyle(fontSize: 16, color: Colors.blue),
-    ),
-    // Display the masked card numbers
-    ...userCreditCards.map((card) => RadioListTile(
-      title: Text(card.maskedCardNumber), // Use maskedCardNumber here
-      value: card.fullCardNumber, // Use fullCardNumber as the value
-      groupValue: selectedCreditCard,
-      onChanged: (value) {
-        setState(() {
-          selectedCreditCard = value.toString();
-          showCVVDialog(card.cvv);
-        });
-      },
-    )),
-  ],
-),
-
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Sélectionnez une carte bancaire :',
+                  style: TextStyle(fontSize: 16, color: Colors.blue),
+                ),
+                // Display the masked card numbers
+                ...userCreditCards.map((card) => RadioListTile(
+                      title: Text(
+                          card.maskedCardNumber), // Use maskedCardNumber here
+                      value: card
+                          .fullCardNumber, // Use fullCardNumber as the value
+                      groupValue: selectedCreditCard,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCreditCard = value.toString();
+                          showCVVDialog(card.cvv);
+                        });
+                      },
+                    )),
+              ],
+            ),
           CheckboxListTile(
             title: const Text('En magasin'),
             value: payInStore,
@@ -427,7 +506,6 @@ Column(
                     });
                   },
                 ),
-                
                 RadioListTile(
                   title: const Text('Carte bancaire'),
                   value: 'credit_card',
@@ -444,7 +522,7 @@ Column(
                   groupValue: selectedPaymentMethod,
                   onChanged: (value) {
                     setState(() {
-                      selectedPaymentMethod = value ;
+                      selectedPaymentMethod = value;
                     });
                   },
                 ),
@@ -456,7 +534,7 @@ Column(
             width: double.infinity,
             color: Colors.green,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: handlePayment,
               style: ElevatedButton.styleFrom(
                 minimumSize: const Size(double.infinity, 50),
                 backgroundColor: Colors.green,
