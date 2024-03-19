fb790bbe77af1421905f4d751ad30b9d64efe9f8
yakine123 <yakinebenali5@gmail.com>
2024-03-19 15:49:21 +0100

    feat:fix `validation de  nom categorie`

--- Backend/controllers/categoriesController.js
+++ Backend/controllers/categoriesController.js
@@ -1,38 +1,38 @@
 const Categories = require('../models/categoriesModel');
 exports.createCategorie = async (req, res) => {
-                    try {
-                      const { nom_cat, type_cat } = req.body;
-                      const existingCategorie = await Categories.findOne({ nom_cat });
+  try {
+    let { nom_cat, type_cat } = req.body;
 
-                      if (existingCategorie) {
-                        
-                        res.json({
-                          status: 400,
-                          message: 'Ce menu existe déjà'
-                        });
-                        return;
-                      }
-                      const newCategorie = new Categories({
-                        nom_cat,
-                        type_cat,
-                        
-                      });
-                  
-                      const savedCategorie = await newCategorie.save();
-    res.json({
+    nom_cat = nom_cat.charAt(0).toUpperCase() + nom_cat.slice(1).toLowerCase();
+    const existingCategorie = await Categories.findOne({ nom_cat });
+
+    if (existingCategorie) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Cette catégorie existe déjà'
+      });
+    }
+    const newCategorie = new Categories({
+      nom_cat,
+      type_cat
+    });
+    const savedCategorie = await newCategorie.save();
+
+    return res.status(200).json({
       status: 200,
-      message: 'Ma categorie a ete cree avec succees ',
+      message: 'La catégorie a été créée avec succès',
       data: savedCategorie,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({
+    return res.status(500).json({
       status: 500,
-      message: 'Erreur lors de la creation de categorie',
+      message: 'Erreur lors de la création de la catégorie',
       error: error.message,
     });
   }
 };
+
                   
 const sendResponse = (res, statusCode, message, data = null) => {
   res.status(statusCode).json({ message, data, status: statusCode });
@@ -40,7 +40,6 @@ const sendResponse = (res, statusCode, message, data = null) => {
 
 exports.getCategories = async (req, res) => {
   try {
-    // Récupération de toutes les catégories depuis la base de données
     const categories = await Categories.find({}, 'nom_cat type_cat id_cat');
     sendResponse(res, 200, "Succès de récupération des catégories", categories);
   } catch (error) {
