2c6fe4a73a6407121c540faf383dfb9e0af854fb
dalel <dalelakremi2020@gmail.com>
2024-03-20 16:35:32 +0100

    feat:add `adminModel`

---
+++ Backend/models/adminModel.js
@@ -0,0 +1,29 @@
+// models/AdminModel.js
+
+const { number } = require('joi');
+const mongoose = require('mongoose');
+
+const AdminSchema = new mongoose.Schema({
+  id_Admin: { type: Number, unique: true },
+  nom: String,
+  prenom: String,
+  telephone:String,
+  email: {type:String,unique:true},
+  password: String,
+  validationCode: String, 
+  validationCodeTimestamp: Date,
+});
+AdminSchema.pre('save', async function (next) {
+  try {
+    if (!this.id_Admin) {
+      const lastAdmin = await this.constructor.findOne({}, {}, { sort: { id_Admin: -1 } });
+      this.id_Admin = lastAdmin ? lastAdmin.id_Admin + 1 : 1;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+const Admin = mongoose.model('Admin', AdminSchema);
+
+module.exports = Admin;
