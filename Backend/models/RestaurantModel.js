c4e43332a5371f9765d095fac3f102abb53dba77
dalel <dalelakremi2020@gmail.com>
2024-03-23 16:40:30 +0100

    feat:add `id` for the `restaurant`

--- Backend/models/RestaurantModel.js
+++ Backend/models/RestaurantModel.js
@@ -1,12 +1,24 @@
 const mongoose = require('mongoose');
 
 const restaurantSchema = new mongoose.Schema({
+  id: { type: Number, unique: true },
   logo: String,
   nom: { type: String, unique: true },
   adresse: { type: String, required: true },
-  ModeDeRetrait: [{ type: String }] // Accepte un tableau de chaînes
+  ModeDeRetrait: [{ type: String }], 
+  ModeDePaiement: [{ type: String }],
 });
-
+restaurantSchema.pre('save', async function (next) {
+  try {
+    if (!this.id) {
+      const lastRestau = await this.constructor.findOne({}, {}, { sort: { id: -1 } });
+      this.id = lastRestau ? lastRestau.id + 1 : 1;
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
 module.exports = Restaurant;
