905005d72be8bd0de8c101b3cf12b9128b140ab9
yakine123 <yakinebenali5@gmail.com>
2024-03-25 23:40:20 +0100

    feat:add `filtre pour mode de paiement`restaurantcontroller.js

--- Backend/controllers/RestaurantController.js
+++ Backend/controllers/RestaurantController.js
@@ -1,39 +1,28 @@
 const Restaurant = require('./../models/RestaurantModel');
-const mongoose = require('mongoose');
 const createRestaurant = async (req, res) => {
     try {
         const { body, file } = req;
         const { nom, adresse, ModeDeRetrait , ModeDePaiement} = body;
-
-        // Vérifier si le nom du restaurant existe déjà
         const existingRestaurant = await Restaurant.findOne({ nom });
         if (existingRestaurant) {
             return res.status(400).json({ success: false, status: 400, message: 'Le nom du restaurant existe déjà' });
         }
-
-        // Construire l'URL de l'image du logo si un fichier est envoyé dans la requête
         const imageUrl = file ? `http://localhost:3000/images/${file.filename}` : null;
 
-        // Diviser la chaîne de modes de retrait par virgules et stocker chaque mode dans un tableau
+       
         const modesDeRetraitArray = ModeDeRetrait.trim().split(',');
-
-        // Supprimez les valeurs vides du tableau
         const filteredModesDeRetraitArray = modesDeRetraitArray.filter(mode => mode.trim() !== '');
         
-        // Utilisez filteredModesDeRetraitArray dans la création du nouveau restaurant
         const modesDePaiementArray = ModeDePaiement.split(',');
+        const filteredmodesDePaiementArray = modesDePaiementArray.filter(mode => mode.trim() !== '');
         const newRestaurant = new Restaurant({
             logo: imageUrl,
             nom,
             adresse,
             ModeDeRetrait: filteredModesDeRetraitArray,
-            ModeDePaiement:modesDePaiementArray
+            ModeDePaiement:filteredmodesDePaiementArray,
         });
-
-        // Sauvegarder le nouveau restaurant dans la base de données
         const savedRestaurant = await newRestaurant.save();
-
-        // Retourner la réponse avec le restaurant ajouté
         res.json({ success: true, status: 200, message: 'Restaurant ajouté avec succès', restaurant: savedRestaurant });
     } catch (error) {
         console.error(error);
@@ -44,14 +33,12 @@ const createRestaurant = async (req, res) => {
 
 const getRestaurant = async (req, res) => {
     try {
-      // Récupérer tous les restaurants
       const restaurants = await Restaurant.find();
   
       if (!restaurants || restaurants.length === 0) {
         return res.status(404).json({ success: false, status: 404, message: 'Aucun restaurant trouvé' });
       }
-  
-      // Renvoyer la liste des restaurants récupérés
+
       res.json({ success: true, status: 200, message: 'Restaurants récupérés avec succès', restaurants });
     } catch (error) {
       console.error(error);
