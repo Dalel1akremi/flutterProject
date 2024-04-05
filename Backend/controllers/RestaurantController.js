7e86b9dd72cf7f53ac624b6558f904a592df8ed3
dalel <dalelakremi2020@gmail.com>
2024-04-05 09:21:04 +0100

    feat:add `getRestaurant by id`

--- Backend/controllers/RestaurantController.js
+++ Backend/controllers/RestaurantController.js
@@ -38,6 +38,23 @@ const createRestaurant = async (req, res) => {
 
 
 
+const getRestau = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({}, 'id_rest'); // Récupère seulement le champ 'id_rest'
+
+    if (!restaurants || restaurants.length === 0) {
+      return res.status(404).json({ success: false, status: 404, message: 'Aucun restaurant trouvé' });
+    }
+
+    // Extrayez uniquement les noms des restaurants
+    const restaurantIds = restaurants.map(restaurant => restaurant.id_rest);
+
+    res.json({ success: true, status: 200, message: 'Noms des restaurants récupérés avec succès', restaurantIds });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, status: 500, message: 'Erreur lors de la récupération des noms des restaurants' });
+  }
+};
 
 const getRestaurant = async (req, res) => {
     try {
@@ -58,5 +75,6 @@ const getRestaurant = async (req, res) => {
 module.exports = {
     createRestaurant,
     getRestaurant,
+    getRestau,
    
 };
