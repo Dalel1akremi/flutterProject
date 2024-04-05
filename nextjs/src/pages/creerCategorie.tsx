49e0f9ba97beb29e54cef50c669603801190ea86
dalel <dalelakremi2020@gmail.com>
2024-04-05 09:30:54 +0100

    feat:add `creer Categorie` page

---
+++ nextjs/src/pages/creerCategorie.tsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import Navbar from '../styles/navbar';
+import jwt from 'jsonwebtoken'; // Importer la bibliothèque jwt pour décoder le token
+import React from 'react';
+
+const CreateCategoriePage = () => {
+  const router = useRouter();
+  const [nomCat, setNomCat] = useState('');
+  const [message, setMessage] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/connexion');
+    }
+  }, []);
+
+  const handleCreateCategorie = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (token) {
+        const decodedToken = jwt.decode(token) as { [key: string]: any };
+        const { id_rest } = decodedToken; // Récupérer l'ID du restaurant du token
+  
+        const nomCatNormalized = nomCat.charAt(0).toUpperCase() + nomCat.slice(1).toLowerCase();
+  
+        const response = await axios.post('http://localhost:3000/createCategorie', {
+          nom_cat: nomCatNormalized,
+          id_rest: id_rest, // Définir l'ID du restaurant dans la catégorie
+        });
+  
+        if (response.data.status === 200) {
+          setMessage('Catégorie créée avec succès');
+          setIsError(false); // Définir isError à false pour indiquer un succès
+        } else {
+          setMessage(response.data.message);
+          setIsError(true); // Définir isError à true pour indiquer une erreur
+        }
+      } // Fermer la condition if (token)
+    } catch (error: any) {
+      setIsError(true); // Définir isError à true pour indiquer une erreur
+      if (error.response && error.response.data.status === 400 && error.response.data.message === 'Cette catégorie existe déjà') {
+        setMessage('erreur,Cette catégorie existe déjà');
+      } else {
+        setMessage('Une erreur est survenue lors de la création de la catégorie');
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div style={{ maxWidth: '400px', margin: 'auto', textAlign: 'center' }}>
+        <h1 style={{ marginBottom: '20px' }}>Créer une nouvelle catégorie</h1>
+        <input
+          type="text"
+          placeholder="Nom de la catégorie"
+          value={nomCat}
+          onChange={e => setNomCat(e.target.value)}
+          style={{ marginBottom: '10px', padding: '8px', borderRadius: '5px', border: '1px solid #ccc', width: '100%' }}
+        />
+
+        <button onClick={handleCreateCategorie} style={{ padding: '10px 20px', borderRadius: '5px', background: '#007bff', color: '#fff', border: 'none', cursor: 'pointer' }}>Créer</button>
+        
+        {/* Utilisation d'une classe CSS dynamique en fonction de isError */}
+        <div style={{ marginTop: '20px', backgroundColor: message.includes('succès') ? 'green' : message.includes('erreur') ? 'red' : 'white', padding: '10px', borderRadius: '5px', color: 'white' }}>
+  {message}
+</div>
+
+
+      </div>
+    </div>
+  );
+};
+
+
+export default CreateCategoriePage;
