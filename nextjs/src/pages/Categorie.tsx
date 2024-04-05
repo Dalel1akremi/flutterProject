1f64fc816a61d55dc98f323e47ebb002d4185267
dalel <dalelakremi2020@gmail.com>
2024-04-05 09:43:51 +0100

    feat:fix `categorie.tsx`

--- nextjs/src/pages/Categorie.tsx
+++ nextjs/src/pages/Categorie.tsx
@@ -1,69 +1,130 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 import { useRouter } from 'next/router';
-import Navbar from '../styles/navbar';
+import Link from 'next/link';
+import Navbar from '@/styles/navbar';
 
-const CreateCategoriePage = () => {
+interface Category {
+  _id: string;
+  nom_cat: string;
+  id_cat: number;
+  is_archived: boolean;
+}
+
+const CategoriesPage = () => {
   const router = useRouter();
-  const [nomCat, setNomCat] = useState('');
-  const [typeCat, setTypeCat] = useState('');
-  const [message, setMessage] = useState('');
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [editingCategoryId, setEditingCategoryId] = useState<string>('');
+  const [newCategoryName, setNewCategoryName] = useState<string>('');
+  const [idRest, setIdRest] = useState<string>('');
+
   useEffect(() => {
-    // Vérifier si l'utilisateur est connecté lors du chargement de la page
     const token = localStorage.getItem('token');
-    if (!token) {
-      // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+    if (token) {
+      const decodedToken = jwt.decode(token) as { [key: string]: any };
+      const { id_rest } = decodedToken;
+      setIdRest(id_rest);
+      getCategories(id_rest);
+    } else {
       router.push('/connexion');
     }
   }, []);
-  const handleCreateCategorie = async () => {
+
+  const getCategories = async (id_rest: string) => {
     try {
-      const nomCatNormalized = nomCat.charAt(0).toUpperCase() + nomCat.slice(1).toLowerCase();
-  
-      const response = await axios.post('http://localhost:3000/createCategorie', {
-        nom_cat: nomCatNormalized,
-        type_cat: typeCat
-      });
-      
-      if (response.data.status === 200) {
-        setMessage('Catégorie créée avec succès');
-      } else {
-        setMessage(response.data.message);
-      }
-    } catch (error: any) { 
-      if (error.response && error.response.data.status === 400 && error.response.data.message === 'Cette catégorie existe déjà') {
-        setMessage('Cette catégorie existe déjà');
-      } else {
-        setMessage('Une erreur est survenue lors de la création de la catégorie');
-      }
+      const response = await axios.get(`http://localhost:3000/getCategories?id_rest=${id_rest}`);
+      setCategories(response.data.data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des catégories :', error);
+    }
+  };
+
+  const handleArchiveToggle = async (id_cat: string) => {
+    try {
+      await axios.put(`http://localhost:3000/ArchivedCategorie/${id_cat}`, {});
+      // Refresh categories after update
+      getCategories(idRest);
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour de l\'état is_archived :', error);
+    }
+  };
+
+  const handleEditCategory = (id_cat: string, nom_cat: string) => {
+    setEditingCategoryId(id_cat);
+    setNewCategoryName(nom_cat);
+  };
+
+  const handleUpdateCategory = async (id_cat: string) => {
+    try {
+      await axios.put(`http://localhost:3000/updateCategory/${id_cat}`, { nom_cat: newCategoryName });
+      // Refresh categories after update
+      getCategories(idRest);
+      setEditingCategoryId('');
+      setNewCategoryName('');
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour de la catégorie :', error);
     }
   };
 
   return (
     <div>
-    
-    <Navbar/>
-    <div style={{ maxWidth: '400px', margin: 'auto', textAlign: 'center' }}>
-      <h1 style={{ marginBottom: '20px' }}>Créer une nouvelle catégorie</h1>
-      <input 
-        type="text" 
-        placeholder="Nom de la catégorie"
-        value={nomCat}
-        onChange={e => setNomCat(e.target.value)}
-        style={{ marginBottom: '10px', padding: '8px', borderRadius: '5px', border: '1px solid #ccc', width: '100%' }}
-      />
-      <input 
-        type="text" 
-        placeholder="Type de la catégorie"
-        value={typeCat}
-        onChange={e => setTypeCat(e.target.value)}
-        style={{ marginBottom: '10px', padding: '8px', borderRadius: '5px', border: '1px solid #ccc', width: '100%' }}
-      />
-      <button onClick={handleCreateCategorie} style={{ padding: '10px 20px', borderRadius: '5px', background: '#007bff', color: '#fff', border: 'none', cursor: 'pointer' }}>Créer</button>
-      {message && <p style={{ marginTop: '20px', color: 'red' }}>{message}</p>}
-    </div>
+      <Navbar />
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '20px' }}>
+          <h1>Liste des catégories</h1>
+          <Link href="/creerCategorie">
+            <button style={{ backgroundColor: 'green', color: 'white', border: 'none', padding: '10px 20px', borderRadius: '5px', cursor: 'pointer' }}>+</button>
+          </Link>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>ID</th>
+              <th>Statut</th>
+              <th>Modification</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map(category => (
+              <tr key={category.id_cat.toString()}>
+                <td>
+  {editingCategoryId === category._id  ? (
+    <input 
+      type="text" 
+      value={newCategoryName} 
+      onChange={e => setNewCategoryName(e.target.value)} 
+    />
+  ) : (
+    category.nom_cat
+  )}
+</td>
+
+                <td>{category.id_cat.toString()}</td>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={category.is_archived}
+                    onChange={() => handleArchiveToggle(category.id_cat.toString())}
+                    style={{ cursor: 'pointer' }}
+                    className={category.is_archived ? 'redCheckbox' : ''}
+                  />
+                </td>
+                <td>
+                  {editingCategoryId === category._id ? (
+                    <button onClick={() => handleUpdateCategory(category.id_cat.toString())} style={{ backgroundColor: 'green', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer' }}>Enregistrer</button>
+                  ) : (
+                    <button onClick={() => handleEditCategory(category._id, category.nom_cat)} style={{ backgroundColor: 'blue', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '5px', cursor: 'pointer' }}>Modifier</button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
-export default CreateCategoriePage;
+export default CategoriesPage;
