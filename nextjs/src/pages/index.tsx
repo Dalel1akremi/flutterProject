7ee04de258956370f722b4c1690e0f8280052a3f
dalel <dalelakremi2020@gmail.com>
2024-04-08 05:34:02 +0100

    add `Navbar` in `acceuil` page

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,8 +1,10 @@
 // pages/index.tsx
+import Navbar from '@/styles/navbar';
 import Connexion from './connexion'
 const IndexPage = () => {
   return (
     <div>
+      <Navbar/>
       <h1>Bonjour mes chers restaurateurs</h1>
          <Connexion/>
      
