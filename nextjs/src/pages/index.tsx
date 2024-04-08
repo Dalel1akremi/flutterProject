0a735bda134efd344723807b5debf1c73e373531
dalel <dalelakremi2020@gmail.com>
2024-04-08 06:28:35 +0100

    fix `index`  acceuil page

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,13 +1,24 @@
 // pages/index.tsx
+import React, { useEffect } from 'react';
 import Navbar from '@/styles/navbar';
-import Connexion from './connexion'
+import Connexion from './connexion';
+import Commandes from './commandes';
+
 const IndexPage = () => {
+  useEffect(() => {
+    // Vérifier si un token est présent dans le stockage
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Rediriger vers la page des commandes si un token est trouvé
+      window.location.href = '/commandes';
+    }
+  }, []);
+
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1>Bonjour mes chers restaurateurs</h1>
-         <Connexion/>
-     
+      <Connexion />
     </div>
   );
 };
