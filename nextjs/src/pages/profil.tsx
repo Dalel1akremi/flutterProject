5a7b2e0f7a40a7f6e5b47c179dee396841095190
dalel <dalelakremi2020@gmail.com>
2024-04-08 05:36:22 +0100

    add `profil` page  to `get` and `update` profil admin

---
+++ nextjs/src/pages/profil.tsx
@@ -0,0 +1,143 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import { useRouter } from 'next/router';
+import Navbar from '@/styles/navbar';
+
+interface AdminData {
+  nom: string;
+  prenom: string;
+  telephone: number;
+  email: string;
+  id_rest: string;
+}
+
+const Profile = () => {
+  const [adminData, setAdminData] = useState<AdminData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [editedNom, setEditedNom] = useState('');
+  const [editedPrenom, setEditedPrenom] = useState('');
+  const [editedTelephone, setEditedTelephone] = useState('');
+  const [editedEmail, setEditedEmail] = useState('');
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const fetchAdmin = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          throw new Error('Token not found');
+        }
+
+        const decodedToken = jwt.decode(token) as { [key: string]: any };
+        const { email } = decodedToken;
+
+        const response = await axios.get(`http://localhost:3000/getAdminByEmail?email=${email}`);
+        setAdminData(response.data); // Supposons que l'API renvoie directement les données de l'administrateur
+        setLoading(false);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+        setError('Une erreur est survenue lors de la récupération des données.');
+        setLoading(false);
+      }
+    };
+
+    fetchAdmin();
+  }, []);
+
+  const handleNomChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedNom(event.target.value);
+  };
+
+  const handlePrenomChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedPrenom(event.target.value);
+  };
+
+  const handleTelephoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTelephone(event.target.value);
+  };
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedEmail(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      if (!adminData) {
+        throw new Error('Aucune donnée d\'administrateur trouvée');
+      }
+  
+      const response = await axios.post(`http://localhost:3000/updateAdmin?email=${adminData.email}`, {
+        nom: editedNom,
+        prenom: editedPrenom,
+        telephone: editedTelephone,
+        email: editedEmail, // Utilisez la valeur de l'email modifiée
+      });
+      console.log(response.data); // Afficher la réponse du serveur
+      setSuccessMessage('Modifications enregistrées avec succès');
+      setTimeout(() => {
+        setSuccessMessage(null);
+        router.push('/connexion'); // Naviguer vers la page d'accueil après 1000ms
+      }, 1000);
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour de l\'administrateur :', error);
+    }
+  };
+                  
+  const renderForm = () => {
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+  
+    if (error) {
+      return <div>Erreur: {error}</div>;
+    }
+  
+    if (!adminData) {
+      return <div>Aucune donnée d'administrateur trouvée.</div>;
+    }
+  
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <h1>Profil de l'administrateur</h1>
+          <form className="form">
+            <div className="formGroup">
+              <label className="input-label">Nom:</label>
+              <input className="input" type="text" value={editedNom || adminData.nom} onChange={handleNomChange} />
+            </div>
+            <div className="formGroup">
+              <label className="input-label">Prénom:</label>
+              <input className="input" type="text" value={editedPrenom || adminData.prenom} onChange={handlePrenomChange} />
+            </div>
+            <div className="formGroup">
+              <label className="input-label">Téléphone:</label>
+              <input className="input" type="text" value={editedTelephone || adminData.telephone} onChange={handleTelephoneChange} />
+            </div>
+            <div className="formGroup">
+              <label className="input-label">Email:</label>
+              <input className="input" type="email" value={editedEmail || adminData.email} onChange={handleEmailChange} />
+            </div>
+            <div className="formGroup">
+              <label className="input-label">ID Restaurant:</label>
+              <input className="input" type="text" value={adminData.id_rest} readOnly />
+            </div>
+            <button type="button" onClick={handleSubmit}>Enregistrer les modifications</button>
+            {successMessage && <div style={{ backgroundColor: 'lightgreen', padding: '10px', marginTop: '10px' }}>{successMessage}</div>}
+          </form>
+        </div>
+      </div>
+    );
+  };
+                  
+  return (
+    <div>
+      {renderForm()}
+    </div>
+  );
+};
+
+export default Profile;
