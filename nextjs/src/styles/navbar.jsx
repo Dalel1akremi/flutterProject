941f1fc19e18353028023d3f4cb2301b9812b193
yakine123 <yakinebenali5@gmail.com>
2024-04-20 17:44:08 +0100

    fix navbar

--- nextjs/src/styles/navbar.jsx
+++ nextjs/src/styles/navbar.jsx
@@ -3,29 +3,23 @@ import axios from 'axios';
 import AcmeLogo from "./AcmeLogo.jsx";
 import styles from './Navbar.module.css';
 import jwt from 'jsonwebtoken';
+import { useRouter } from 'next/router'; 
 
 export default function Navbar() {
   const [restaurantName, setRestaurantName] = useState('');
+  const router = useRouter();
 
   useEffect(() => {
     const fetchRestaurantName = async () => {
       try {
-        // Récupérer le token du localStorage
         const token = localStorage.getItem('token');
         if (!token) {
           throw new Error('Token not found in localStorage');
         }
-  
-        // Décoder le token pour obtenir l'id_rest
         const decodedToken = jwt.decode(token);
         const { id_rest: decodedRestaurantId } = decodedToken;
-  
-        // Faire l'appel à l'API pour récupérer les restaurants
         const response = await axios.get('http://localhost:3000/getRestau');
-        console.log('Response from API:', response.data); // Afficher la réponse de l'API dans la console
         const { restaurants } = response.data;
-        
-        // Trouver le restaurant correspondant à l'ID décodé
         const matchedRestaurant = restaurants.find(restaurant => restaurant.id_rest === decodedRestaurantId);
         if (matchedRestaurant) {
           setRestaurantName(matchedRestaurant.nom);
@@ -39,8 +33,9 @@ export default function Navbar() {
   
     fetchRestaurantName();
   }, []);
-  
-  
+
+  const shouldDisplayRestaurantLink = router.pathname === 'admin/Restaurant';
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.left}>
@@ -48,14 +43,16 @@ export default function Navbar() {
         <div className={styles.logo}>{restaurantName || 'Nom du Restaurant'}</div>
       </div>
       <div className={styles.center}>
-        <a href="/" className={styles.link}>Accueil</a>
         <a href="/Categories" className={styles.link}>Catégories</a>
         <a href="/produits" className={styles.link}>Produits</a>
         <a href="/commandes" className={styles.link}>Commandes</a>
         <a href="/Elements" className={styles.link}>Elements</a>
         <a href="/profil" className={styles.link}>Profil</a>
-       
+        
 
+        {shouldDisplayRestaurantLink && (
+          <a href="/Restaurant" className={styles.link}>Restauant</a>
+        )}
       </div>
       <div className={styles.right}>
         <a href="/deconnexion" className={styles.link}>Se déconnecter</a>
