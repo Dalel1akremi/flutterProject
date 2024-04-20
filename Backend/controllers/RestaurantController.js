80dbc44886289ec4f5f7406099861c52870a283c
yakine123 <yakinebenali5@gmail.com>
2024-04-20 17:40:56 +0100

    feat:fix createRestaurant

--- Backend/controllers/RestaurantController.js
+++ Backend/controllers/RestaurantController.js
@@ -4,12 +4,16 @@ const createRestaurant = async (req, res) => {
     const { body, files } = req; 
     const { nom, adresse, ModeDeRetrait, ModeDePaiement, numero_telephone, email } = body;
   
-    const existingRestaurant = await Restaurant.findOne({ $or: [{ nom }, { email }] });
+    const existingRestaurant = await Restaurant.findOne({nom});
     
     if (existingRestaurant) {
-      return res.status(400).json({ success: false, status: 400, message: 'Le nom ou l\'e-mail du restaurant existe déjà' });
+      return res.status(400).json({ success: false, status: 400, message: 'Le nom  du restaurant existe déjà' });
     }
-   
+    const existingEmail = await Restaurant.findOne({email});
+    
+    if (existingEmail) {
+      return res.status(400).json({ success: false, status: 400, message: ' l\'e-mail du restaurant existe déjà' });
+    }
     const logoUrl = files.logo ? `http://localhost:3000/images/${files.logo[0].filename}` : null;
     const imageUrl = files.image ? `http://localhost:3000/images/${files.image[0].filename}` : null;
 
@@ -40,14 +44,12 @@ const createRestaurant = async (req, res) => {
 
 const getRestau = async (req, res) => {
   try {
-    // Récupérer tous les restaurants avec leurs noms et id_rest
     const restaurants = await Restaurant.find({}, 'id_rest nom email');
 
     if (!restaurants || restaurants.length === 0) {
       return res.status(404).json({ success: false, status: 404, message: 'Aucun restaurant trouvé' });
     }
 
-    // Extrayez à la fois les noms et les id_rest des restaurants
     const restaurantData = restaurants.map(restaurant => ({
       id_rest: restaurant.id_rest,
       nom: restaurant.nom,
