fcf8f64ed296c1856d4f3592d5fd2d9e6adb13f3
dalel <dalelakremi2020@gmail.com>
2024-04-25 10:08:47 +0100

    Update CategoryPage in Flutter app

--- FLUTTER/lib/pages/aboutRestaurant/CategoryPage.dart
+++ FLUTTER/lib/pages/aboutRestaurant/CategoryPage.dart
@@ -16,13 +16,11 @@ class NextPage extends StatefulWidget {
   final List<Article> panier;
 
   const NextPage({
-    super.key,
-
+    Key? key,
     required this.panier,
-  });
+  }) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _NextPageState createState() => _NextPageState();
 }
 
@@ -38,21 +36,19 @@ class _NextPageState extends State<NextPage> {
 
   Future<void> fetchCategories() async {
     try {
-         final int? idRest = Panier().getIdRestaurant();
-    if (idRest == null) {
-      throw Exception('Restaurant ID is null');
-    }
-      final response =  await http.get(Uri.parse('http://192.168.2.65:3000/getCategories?id_rest=$idRest'));
+      final int? idRest = Panier().getIdRestaurant();
+      if (idRest == null) {
+        throw Exception('Restaurant ID is null');
+      }
+      final response = await http.get(Uri.parse('http://192.168.2.65:3000/getCategories?id_rest=$idRest'));
 
       if (response.statusCode == 200) {
         final List<dynamic> responseData = json.decode(response.body)['data'];
         setState(() {
-          _categories =
-              responseData.map((json) => Category.fromJson(json)).toList();
+          _categories = responseData.map((json) => Category.fromJson(json)).toList();
         });
       } else {
-        throw Exception(
-            'Failed to fetch categories. Status code: ${response.statusCode}');
+        throw Exception('Failed to fetch categories. Status code: ${response.statusCode}');
       }
     } catch (error) {
       if (kDebugMode) {
@@ -60,40 +56,39 @@ class _NextPageState extends State<NextPage> {
       }
     }
   }
-Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
-  try {
-    final int? idRest = Panier().getIdRestaurant();
-    if (idRest == null) {
-      throw Exception('Restaurant ID is null');
-    }
-  final response = await http.get(Uri.parse('http://192.168.2.65:3000/getItem?id_cat=$idCat&id_rest=$idRest'));
 
+  Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
+    try {
+      final int? idRest = Panier().getIdRestaurant();
+      if (idRest == null) {
+        throw Exception('Restaurant ID is null');
+      }
+      final response = await http.get(Uri.parse('http://192.168.2.65:3000/getItem?id_cat=$idCat&id_rest=$idRest'));
 
-    if (response.statusCode == 200) {
-      final List<dynamic>? responseData = json.decode(response.body)['formattedItems'];
-  
-      if (responseData != null && responseData.isNotEmpty) {
-        return responseData.map<Map<String, dynamic>>((item) => item as Map<String, dynamic>).toList();
+      if (response.statusCode == 200) {
+        final List<dynamic>? responseData = json.decode(response.body)['formattedItems'];
+
+        if (responseData != null && responseData.isNotEmpty) {
+          return responseData.map<Map<String, dynamic>>((item) => item as Map<String, dynamic>).toList();
+        } else {
+          if (kDebugMode) {
+            print('Error fetching menu: Response data is null or empty');
+          }
+          return [];
+        }
       } else {
         if (kDebugMode) {
-          print('Error fetching menu: Response data is null or empty');
+          print('Error fetching menu. Status code: ${response.statusCode}');
         }
-        return [];
+        throw Exception('Failed to fetch menu. Status code: ${response.statusCode}');
       }
-    } else {
+    } catch (error) {
       if (kDebugMode) {
-        print('Error fetching menu. Status code: ${response.statusCode}');
+        print('Error fetching menu: $error');
       }
-      throw Exception('Failed to fetch menu. Status code: ${response.statusCode}');
+      return [];
     }
-  } catch (error) {
-    if (kDebugMode) {
-      print('Error fetching menu: $error');
-    }
-    return [];
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -104,50 +99,50 @@ Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
       numberOfItems += article.quantite;
     }
     String? restaurantName = Panier().getSelectedRestaurantName();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(222, 212, 133, 14),
-         title: Text(restaurantName ?? 'Restaurant Detail'),
+        title: Text(restaurantName ?? 'Restaurant Detail'),
         leading: IconButton(
-  icon: const Icon(Icons.arrow_back),
-  onPressed: () {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Confirmation'),
-          content: const Text('Vous avez des articles dans votre panier. Voulez-vous le vider ?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child:const Text('Non'),
-            ),
-            TextButton(
-              onPressed: () {
-                Panier().viderPanier();
-                 Navigator.of(context).pop(); 
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const RestaurantDetail(),
-                  ),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Confirmation'),
+                  content: const Text('Vous avez des articles dans votre panier. Voulez-vous le vider ?'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Non'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Panier().viderPanier();
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const RestaurantDetail(),
+                          ),
+                        );
+                      },
+                      child: const Text('Oui'),
+                    ),
+                  ],
                 );
               },
-              child: const Text('Oui'),
-            ),
-          ],
-        );
-      },
-    );
-  },
-),
-
+            );
+          },
+        ),
       ),
       body: Column(
         children: [
           SizedBox(
-            height: 56,
+            height: 90,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: _categories.length,
@@ -159,26 +154,23 @@ Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
                     });
                   },
                   child: Container(
-                    width:
-                        MediaQuery.of(context).size.width / _categories.length,
+                    width: MediaQuery.of(context).size.width / _categories.length,
                     padding: const EdgeInsets.symmetric(horizontal: 16),
-                    color: _selectedCategoryIndex == index
-                        ? Colors.grey[200]
-                        : Colors.white,
+                    color: _selectedCategoryIndex == index ? Colors.grey[200] : Colors.white,
                     child: Center(
-                      child: Row(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(Icons.restaurant_menu,
-                              color: _selectedCategoryIndex == index
-                                  ? Colors.purple
-                                  : Colors.black),
-                          const SizedBox(width: 8),
+                          Image.network(
+                            _categories[index].imageUrl,
+                            width: 50,
+                            height: 50,
+                          ),
+                          const SizedBox(height: 8),
                           Text(
                             _categories[index].nomCat,
                             style: TextStyle(
-                              color: _selectedCategoryIndex == index
-                                  ? Colors.purple
-                                  : Colors.black,
+                              color: _selectedCategoryIndex == index ? Colors.purple : Colors.black,
                             ),
                           ),
                         ],
@@ -191,106 +183,101 @@ Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
           ),
           Expanded(
             child: Center(
-              child: _buildMenuForCategory(_categories.isNotEmpty
-                  ? _categories[_selectedCategoryIndex]
-                  : Category(idCat: 1, nomCat: '')),
+              child: _buildMenuForCategory(_categories.isNotEmpty ? _categories[_selectedCategoryIndex] : Category(idCat: 1, nomCat: '', imageUrl: '')),
             ),
           ),
-       Padding(
-  padding: const EdgeInsets.all(16.0),
-  child: ElevatedButton(
-    onPressed: () {
-      if (totalPrice > 0) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const PanierPage(),
-          ),
-        );
-      } 
-    },
-    style: ElevatedButton.styleFrom(
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (totalPrice > 0) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const PanierPage(),
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
                 minimumSize: const Size(150, 50),
                 backgroundColor: Colors.green,
               ),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          ' $numberOfItems article${numberOfItems != 1 ? 's' : ''}',
-          style: const TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    ' $numberOfItems article${numberOfItems != 1 ? 's' : ''}',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Text(
+                    'Paiement',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    ' $totalPrice €',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-        const Text(
-          'Paiement',
-          style: TextStyle(color: Colors.white),
-        ),
-        Text(
-          ' $totalPrice €',
-          style: const TextStyle(
-            fontSize: 12,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
-      ],
-    ),
-  ),
-)
-,
         ],
       ),
     );
   }
 
   Widget _buildMenuForCategory(Category category) {
-   return FutureBuilder<List<Map<String?, dynamic>>>(
-  future: fetchMenu(category.idCat),
-  builder: (context, snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return const CircularProgressIndicator();
-    } else if (snapshot.hasError) {
-      return Text('Error loading menu: ${snapshot.error}');
-    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-      return const Text('No menu items available.');
-    } else {
-      return ListView(
-        children: snapshot.data!.map<Widget>((menuItem) {
+    return FutureBuilder<List<Map<String?, dynamic>>>(
+      future: fetchMenu(category.idCat),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error loading menu: ${snapshot.error}');
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Text('No menu items available.');
+        } else {
+          return ListView(
+            children: snapshot.data!.map<Widget>((menuItem) {
               return GestureDetector(
                 onTap: () {
                   if (menuItem['is_Redirect'] == true) {
-                      final int? idRest = Panier().getIdRestaurant();
-    if (idRest == null) {
-      throw Exception('Restaurant ID is null');
-    }
+                    final int? idRest = Panier().getIdRestaurant();
+                    if (idRest == null) {
+                      throw Exception('Restaurant ID is null');
+                    }
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => ItemDetailsPage(
-                           id_item: menuItem['id_item'],
-                          nom: menuItem['nom']?? 'Default Name',
+                          id_item: menuItem['id_item'],
+                          nom: menuItem['nom'] ?? 'Default Name',
                           img: menuItem['image'],
                           prix: menuItem['prix'],
-                           id_Steps:menuItem['id_Steps'] ?? [],
-                           id_rest: menuItem['idRest']??0,
-            
+                          id_Steps: menuItem['id_Steps'] ?? [],
+                          id_rest: menuItem['idRest'] ?? 0,
                         ),
                       ),
                     );
                   } else {
-               
                     Navigator.push(
                       context,
                       MaterialPageRoute(
                         builder: (context) => StepMenuPage(
-                         id_item: menuItem['id_item'],
+                          id_item: menuItem['id_item'],
                           nom: menuItem['nom'],
                           img: menuItem['image'],
                           prix: menuItem['prix'],
-                          id_Steps:menuItem['id_Steps'] ?? [],
+                          id_Steps: menuItem['id_Steps'] ?? [],
                         ),
                       ),
                     );
@@ -324,14 +311,13 @@ Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
                               ),
                             ),
                             const SizedBox(height: 8),
-                             Text(
-                            menuItem['description'] ?? '', 
-                            style: const TextStyle(
-                              fontWeight: FontWeight.normal,
-                              fontSize: 16,
+                            Text(
+                              menuItem['description'] ?? '',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 16,
+                              ),
                             ),
-                          ),
-                            
                             const SizedBox(height: 8),
                             if (!(menuItem['is_Redirect'] == true))
                               Text(
@@ -355,26 +341,33 @@ Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
     );
   }
 }
+
 class Category {
   final int idCat;
   final String nomCat;
+  final String imageUrl;
 
   Category({
     required this.idCat,
     required this.nomCat,
+    required this.imageUrl,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) {
     final int? categoryId = json['id_cat'] as int?;
     final String? categoryNomCat = json['nom_cat'] as String?;
-
-    if (categoryId != null && categoryNomCat != null) {
-      return Category(idCat: categoryId, nomCat: categoryNomCat);
+    final String? categoryImageUrl = json['image'] as String?;
+    if (categoryId != null && categoryNomCat != null && categoryImageUrl != null) {
+      return Category(
+        idCat: categoryId,
+        nomCat: categoryNomCat,
+        imageUrl: categoryImageUrl,
+      );
     } else {
       if (kDebugMode) {
-        print("Warning: 'id_cat' or 'nom_cat' is null in JSON data. Using default values.");
+        print("Warning: 'id_cat', 'nom_cat' or 'image_url' is null in JSON data. Using default values.");
       }
-      return Category(idCat: 0, nomCat: 'Default Category');
+      return Category(idCat: 0, nomCat: 'Default Category', imageUrl: '');
     }
   }
 }
