33a1ed8d03354ea03b4ce26000b2ffc5658f793d
yakine123 <yakinebenali5@gmail.com>
2024-04-28 23:17:12 +0100

    fix:`tous le page redirecte vers page d'acceuil`

--- FLUTTER/lib/main.dart
+++ FLUTTER/lib/main.dart
@@ -5,18 +5,16 @@ import './pages/aboutUser/auth_provider.dart';
 import './pages/aboutRestaurant/acceuil.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-     apiKey: "AIzaSyBB0RXK1lHpKPfmD_a-L3OtaOzJBpG1SbY",
-  authDomain: "speedy-cedar-413809.firebaseapp.com",
-  projectId: "speedy-cedar-413809",
-  storageBucket: "speedy-cedar-413809.appspot.com",
-  messagingSenderId: "800045568375",
-  appId: "1:800045568375:web:db95405a19d6d393f55172",
-
+      apiKey: "AIzaSyBB0RXK1lHpKPfmD_a-L3OtaOzJBpG1SbY",
+      authDomain: "speedy-cedar-413809.firebaseapp.com",
+      projectId: "speedy-cedar-413809",
+      storageBucket: "speedy-cedar-413809.appspot.com",
+      messagingSenderId: "800045568375",
+      appId: "1:800045568375:web:db95405a19d6d393f55172",
     ),
   );
   runApp(const MyApp());
@@ -29,8 +27,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-         ChangeNotifierProvider(create: (_) => AuthProvider()), 
-         ChangeNotifierProvider(create: (_) => CommandesModel()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CommandesModel()),
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -57,13 +55,23 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/acceuil.jpeg'),
-                fit: BoxFit.cover,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AcceuilScreen(),
+                ),
+              );
+            },
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/acceuil.jpeg'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
@@ -71,37 +79,38 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Text(
+              Text(
                 'Découvrir',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 30, color: Colors.white),
               ),
-              const SizedBox(height: 10),
-              const Text(
+              SizedBox(height: 10),
+              Text(
                 'Rechercher les restaurants les mieux notés à proximité de votre région',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 18, color: Colors.white),
               ),
-              const SizedBox(height: 30),  
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AcceuilScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black.withOpacity(0.07), 
-                ),
-                child: const Text(
-                  'Commencer',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              SizedBox(height: 30),
+             Center(
+  child: GestureDetector(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const AcceuilScreen(),
+        ),
+      );
+    },
+    child: Text(
+      'Commencer',
+      style: TextStyle(
+        fontSize: 28,
+        color: Colors.white,
+      ),
+    ),
+  ),
+),
+
             ],
           ),
         ],
