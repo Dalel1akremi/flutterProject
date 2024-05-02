679a8bc5174989cadd6260e434d46b7c2ca126d9
dalel <dalelakremi2020@gmail.com>
2024-05-02 10:49:44 +0100

    Replace `Navigator.pushReplacement `  with `Navigator.push` in  `paiement.dart`

--- FLUTTER/lib/pages/aboutPaiement/paiement.dart
+++ FLUTTER/lib/pages/aboutPaiement/paiement.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_print, library_private_types_in_public_api, use_build_context_synchronously
 import 'dart:convert';
-import 'package:demo/pages/aboutRestaurant/commande.dart';
+import 'package:demo/pages/aboutRestaurant/RestaurantList.dart';
 import 'package:demo/pages/aboutUser/adresse.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -619,10 +619,11 @@ Future<void> compareCVV(String hashedCVV, String enteredCVV) async {
 
           if (paymentHandled) {
             Panier().viderPanier();
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => const CommandeApp()),
-            );
+            panier.origine = 'commandes';
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const RestaurantScreen(index: 2,)),
+          );
           }
         },
         style: ElevatedButton.styleFrom(
