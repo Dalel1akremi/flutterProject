54f9d77e895b7a1ad25e0c650f2b2691f1c7f71f
dalel <dalelakremi2020@gmail.com>
2024-05-03 11:01:06 +0100

    "Refactor CategoryPage layout and update menu item display"

--- FLUTTER/lib/pages/aboutRestaurant/CategoryPage.dart
+++ FLUTTER/lib/pages/aboutRestaurant/CategoryPage.dart
@@ -110,65 +110,72 @@ class _NextPageState extends State<NextPage> {
       backgroundColor: Colors.white,
       body: Column(
         children: [
-Container(
-  height: 80,
-  decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(20.0),
-    border: Border.all(color: Colors.black),
-  ),
-  child: SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-      children: List.generate(
-        _categories.length,
-        (index) {
-          return Row(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _selectedCategoryIndex = index;
-                  });
-                },
-                child: Container(
-                  width: 150, 
-                  height: 120, 
-                  margin: const EdgeInsets.symmetric(horizontal: 4), // Add some spacing between cards
-                  child: Card(
-                    elevation: _selectedCategoryIndex == index ? 8 : 0,
-                    color: _selectedCategoryIndex == index ? Colors.grey[200] : Colors.white,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.network(
-                            _categories[index].imageUrl,
-                            width: 50,
-                            height: 50,
+Padding(
+  padding: const EdgeInsets.only(top: 16.0), // Ajouter de l'espace en haut
+  child: Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16.0), // Ajouter de l'espace à gauche et à droite
+    child: Padding(
+      padding: const EdgeInsets.only(bottom: 16.0), // Ajouter de l'espace en bas
+      child: SizedBox(
+        height: 80,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              _categories.length,
+              (index) {
+                return Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _selectedCategoryIndex = index;
+                        });
+                      },
+                      child: Card(
+                        surfaceTintColor: Colors.white,
+                        color: _selectedCategoryIndex == index ? const  Color.fromARGB(255, 252, 240, 222) : Colors.white, 
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8), 
+                          side: const BorderSide(
+                            color: Color.fromARGB(222, 212, 133, 14), // Border color of the card for all categories
+                            width: 2, 
                           ),
-                          const SizedBox(width: 8),
-                          Text(
-                            _categories[index].nomCat,
-                            style: TextStyle(
-                              color: _selectedCategoryIndex == index ? Colors.purple : Colors.black,
-                            ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.network(
+                                _categories[index].imageUrl,
+                                width: 50,
+                                height: 50,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(
+                                _categories[index].nomCat,
+                                style: TextStyle(
+                                  color: _selectedCategoryIndex == index ? Colors.purple : Colors.black,
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ),
-              if (index != _categories.length - 1) 
-                const VerticalDivider(
-                  color: Colors.black,
-                  thickness: 1,
-                  width: 0,
-                ),
-            ],
-          );
-        },
+                    if (index != _categories.length - 1)
+                      const VerticalDivider(
+                        color: Colors.white,
+                        thickness: 1,
+                        width: 0,
+                      ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
       ),
     ),
   ),
@@ -179,8 +186,9 @@ Expanded(
               child: _buildMenuForCategory(_categories.isNotEmpty ? _categories[_selectedCategoryIndex] : Category(idCat: 1, nomCat: '', imageUrl: '')),
             ),
           ),
+          const SizedBox(width: 16),
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(32.0),
             child: ElevatedButton(
               onPressed: () {
                 if (totalPrice > 0) {
@@ -228,111 +236,121 @@ Expanded(
     );
   }
 
-  Widget _buildMenuForCategory(Category category) {
-    return FutureBuilder<List<Map<String?, dynamic>>>(
-      future: fetchMenu(category.idCat),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error loading menu: ${snapshot.error}');
-        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Text('No menu items available.');
-        } else {
-          return ListView(
-            children: snapshot.data!.map<Widget>((menuItem) {
-              return GestureDetector(
-                onTap: () {
-                  if (menuItem['is_Redirect'] == true) {
-                    final int? idRest = Panier().getIdRestaurant();
-                    if (idRest == null) {
-                      throw Exception('Restaurant ID is null');
-                    }
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ItemDetailsPage(
-                          id_item: menuItem['id_item'],
-                          nom: menuItem['nom'] ?? 'Default Name',
-                          img: menuItem['image'],
-                          prix: menuItem['prix'],
-                          id_Steps: menuItem['id_Steps'] ?? [],
-                          id_rest: menuItem['idRest'] ?? 0,
-                        ),
-                      ),
-                    );
-                  } else {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => StepMenuPage(
-                          id_item: menuItem['id_item'],
-                          nom: menuItem['nom'],
-                          img: menuItem['image'],
-                          prix: menuItem['prix'],
-                          id_Steps: menuItem['id_Steps'] ?? [],
-                        ),
-                      ),
-                    );
+Widget _buildMenuForCategory(Category category) {
+  return FutureBuilder<List<Map<String?, dynamic>>>(
+    future: fetchMenu(category.idCat),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator());
+      } else if (snapshot.hasError) {
+        return Text('Error loading menu: ${snapshot.error}');
+      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+        return const Text('No menu items available.');
+      } else {
+        return Padding(
+  padding: const EdgeInsets.only(top: 16.0),
+   child: GridView.builder(
+  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2, 
+    crossAxisSpacing: 6, 
+    mainAxisSpacing: 6, 
+  ),
+  itemCount: snapshot.data!.length,
+  itemBuilder: (context, index) {
+    var menuItem = snapshot.data![index];
+    return GestureDetector(
+      onTap: () {
+         if (menuItem['is_Redirect'] == true) {
+                  final int? idRest = Panier().getIdRestaurant();
+                  if (idRest == null) {
+                    throw Exception('Restaurant ID is null');
                   }
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(181, 237, 231, 231),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network(
-                        menuItem['image'],
-                        width: 150,
-                        height: 100,
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ItemDetailsPage(
+                        id_item: menuItem['id_item'],
+                        nom: menuItem['nom'] ?? 'Default Name',
+                        img: menuItem['image'],
+                        prix: menuItem['prix'],
+                        id_Steps: menuItem['id_Steps'] ?? [],
+                        id_rest: menuItem['idRest'] ?? 0,
                       ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '${menuItem['nom']}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              menuItem['description'] ?? '',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 16,
-                              ),
-                            ),
-                            const SizedBox(height: 8),
-                            if (!(menuItem['is_Redirect'] == true))
-                              Text(
-                                'Prix: ${menuItem['prix']}€',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                          ],
-                        ),
+                    ),
+                  );
+                } else {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => StepMenuPage(
+                        id_item: menuItem['id_item'],
+                        nom: menuItem['nom'],
+                        img: menuItem['image'],
+                        prix: menuItem['prix'],
+                        id_Steps: menuItem['id_Steps'] ?? [],
                       ),
-                    ],
+                    ),
+                  );
+                }
+      },
+      child: Card(
+         surfaceTintColor: Colors.orange.withOpacity(0.4),
+        elevation: 4,
+        margin: const EdgeInsets.all(8),  
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8), 
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  menuItem['image'],
+                  width:120, 
+                  height: 80, 
+                  fit: BoxFit.cover, 
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  '${menuItem['nom']}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14, // Réduisez la taille de la police
                   ),
                 ),
-              );
-            }).toList(),
-          );
-        }
-      },
+                const SizedBox(height: 2), // Réduisez l'espacement entre les éléments
+                Text(
+                  menuItem['description'] ?? '',
+                  maxLines: 2, // Limitez le texte à deux lignes
+                  overflow: TextOverflow.ellipsis, // Ajoutez des points de suspension pour le texte dépassant
+                  style: const TextStyle(
+                    fontSize: 12, // Réduisez la taille de la police
+                  ),
+                ),
+                const SizedBox(height: 2), // Réduisez l'espacement entre les éléments
+                if (!(menuItem['is_Redirect'] == true))
+                  Text(
+                    'Prix: ${menuItem['prix']}€',
+                    style: const TextStyle(
+                      fontSize: 12, // Réduisez la taille de la police
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  },
+));
   }
+    },
+  );
+}
 
   Future<List<Map<String, dynamic>>> fetchMenu(int idCat) async {
     try {
