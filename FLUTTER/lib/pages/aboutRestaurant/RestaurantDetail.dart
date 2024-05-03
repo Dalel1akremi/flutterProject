31a795499ec11a773f23d9484ba03d605dd73148
dalel <dalelakremi2020@gmail.com>
2024-05-03 15:05:00 +0100

    "Refactor RestaurantDetail body: replace SingleChildScrollView with Expanded and Stack for better layout and design"

--- FLUTTER/lib/pages/aboutRestaurant/RestaurantDetail.dart
+++ FLUTTER/lib/pages/aboutRestaurant/RestaurantDetail.dart
@@ -140,112 +140,138 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
       },
     ),
     ),
-      body: SingleChildScrollView(
+      body: Expanded(
         child: Column(
           children: [
-            Container(
-              padding: const EdgeInsets.all(1.0),
-              height: MediaQuery.of(context).size.height / 3,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.white.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-                image: restaurantImage != null
-                    ? DecorationImage(
-                        image: NetworkImage(restaurantImage),
-                        fit: BoxFit.cover,
-                      )
-                    : null,
-              ),
-              child: Stack(
-                children: [
-                  if (restaurantLogo != null)
-                    Positioned(
-                      left: 16.0,
-                      bottom: 1.0,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image.network(
-                          restaurantLogo,
-                          width: 80,
-                          height: 80,
-                          fit: BoxFit.cover,
-                        ),
+            Expanded(
+            child:Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(1.0),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(bottomRight: Radius.circular(50)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: const Offset(0, 3),
                       ),
-                    ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: 1500,
-              height: 100,
-              decoration: BoxDecoration(
-                border:
-                    Border.all(color: const Color.fromARGB(255, 234, 231, 231)),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: const EdgeInsets.all(5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(restaurantAdress ?? 'Restaurant Detail'),
-                  const Text(
-                    'Nous acceptons:',
-                    style: TextStyle(fontSize: 18),
+                    ],
+                    image: restaurantImage != null
+                        ? DecorationImage(
+                            image: NetworkImage(restaurantImage),
+                            fit: BoxFit.cover,
+                          )
+                        : null,
                   ),
-                ],
-              ),
+               ),
+              if (restaurantLogo != null)
+                        Positioned(
+                          left: 16.0,
+                          bottom: -40,
+                          child:  CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.white,
+                            child: Image.network(
+                              restaurantLogo,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+              ],
             ),
-            const SizedBox(height: 5),
-            Container(
-              width: 1500,
-              height: 200,
-              decoration: BoxDecoration(
-                border:
-                    Border.all(color: const Color.fromARGB(255, 234, 231, 231)),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Mode de retrait*:',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: restaurant != null
-                        ? restaurant.split(',').map((mode) {
-                            return Padding(
-                              padding: const EdgeInsets.only(top: 8.0),
-                              child: GlowRadioButton(
-                                label: mode.trim(),
-                                value: mode.trim(),
-                                groupValue: selectedRetraitMode,
-                                onChanged: (value) {
-                                  setState(() {
-                                    selectedRetraitMode = value;
-                                  });
-                                  _selectTime(context);
-                                },
-                              ),
-                            );
-                          }).toList()
-                        : [],
-                  ),
-                ],
-              ),
             ),
-          ],
+            const SizedBox(height: 50),
+SizedBox(
+  width: double.infinity,
+  child: Padding(
+    padding: const EdgeInsets.all(5), // Padding autour des deux cartes
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch, // Ajuste la largeur des enfants pour remplir la largeur disponible
+      children: [
+        Card(
+          surfaceTintColor: Colors.orange.withOpacity(0.4),
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(color:  Color.fromARGB(255, 234, 231, 231)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+    Row(
+      children: [
+        const Icon(
+          Icons.location_on,
+          color: Colors.green, // Couleur de l'icône de localisation
+        ),
+        const SizedBox(width: 5), // Espacement entre l'icône et le texte de l'adresse
+        Text(
+          restaurantAdress ?? 'Restaurant Detail',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    ),
+    const SizedBox(height: 20), 
+  ],
+),
+
+          ),
+        ),
+        const SizedBox(height: 5),
+        Card(
+          surfaceTintColor: Colors.orange.withOpacity(0.4),
+          color: Colors.white,
+          elevation: 5, 
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+            side: const BorderSide(color:  Color.fromARGB(255, 234, 231, 231)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20), 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Mode de retrait*:',
+                  style: TextStyle(fontSize: 20), 
+                ),
+                const SizedBox(height: 17), 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: restaurant != null
+                      ? restaurant.split(',').map((mode) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 12.0),
+                            child: GlowRadioButton(
+                              label: mode.trim(),
+                              value: mode.trim(),
+                              groupValue: selectedRetraitMode,
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedRetraitMode = value;
+                                });
+                                _selectTime(context);
+                              },
+                            ),
+                          );
+                        }).toList()
+                      : [],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  ),
+)
+       ],
         ),
       ),
       bottomNavigationBar: Container(
