3a1139086c5a718ff5f73c937dfd5effbcbb2953
yakine123 <yakinebenali5@gmail.com>
2024-05-15 20:25:15 +0100

    fix:add `acceuil a navbar `et apres la connexion redirect vers page d'acceuil

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -1,24 +1,20 @@
 // pages/index.tsx
 import React, { useEffect } from 'react';
 import Navbar from '@/styles/navbar';
-import Connexion from './connexion';
-import Commandes from './commandes';
+
 
 const IndexPage = () => {
   useEffect(() => {
     // Vérifier si un token est présent dans le stockage
     const token = localStorage.getItem('token');
-    if (token) {
-      // Rediriger vers la page des commandes si un token est trouvé
-      window.location.href = '/commandes';
-    }
+ 
   }, []);
 
   return (
     <div>
       <Navbar />
-      <h1>Bonjour mes chers restaurateurs</h1>
-      <Connexion />
+      <h1>Bonjour me chers restaurateur</h1>
+    
     </div>
   );
 };
